b6745782fa71978044a0bc516c0b929d44fcf470
Babichev Maxim <maksim.babichev95@gmail.com>
2017-01-05 23:47:53 +0300

    add example speed

---
+++ demo/speed.php
@@ -0,0 +1,71 @@
+<?php
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+interface S
+{
+    public function rand();
+}
+
+class S1 extends \Deimos\Builder\Builder implements S
+{
+
+    /**
+     * method Random
+     *
+     * @return int
+     */
+    public function rand()
+    {
+        return $this->once(function ()
+        {
+            return mt_rand();
+        });
+    }
+
+}
+
+class S2 extends \Deimos\Builder\Builder implements S
+{
+
+    /**
+     * method Random
+     *
+     * @return int
+     */
+    public function rand()
+    {
+        return $this->once(function ()
+        {
+            return mt_rand();
+        }, __METHOD__);
+    }
+
+}
+
+$s1 = new S1();
+$s2 = new S2();
+
+function mt(S $s)
+{
+    $range = range(1, 100);
+
+    $mt = microtime(true);
+
+    foreach ($range as $value1)
+    {
+        foreach ($range as $value2)
+        {
+            foreach ($range as $value3)
+            {
+                $s->rand();
+            }
+        }
+    }
+
+    return microtime(true) - $mt;
+}
+
+// banchmark
+var_dump(mt($s1));
+var_dump(mt($s2));
