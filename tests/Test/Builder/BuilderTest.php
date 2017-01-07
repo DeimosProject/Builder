b514d55fe16ee59e567d2173f43f87250647eb02
Babichev Maxim <maksim.babichev95@gmail.com>
2017-01-07 23:01:53 +0300

    add phpunit

---
+++ tests/Test/Builder/BuilderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Builder;
+
+class BuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testEntity()
+    {
+        // todo
+        $this->assertEquals(1, 1);
+    }
+
+}
